e204fed745ffe0adab75af97e95d9b8f7a186c1f
Kavin1992 <kavinkumarskct@gmail.com>
2018-03-05 11:18:46 +0530

    01 fixed

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -1,18 +1,26 @@
 from data import DICTIONARY, LETTER_SCORES
+from test_wordvalue import TestWordValue
 
 def load_words():
-    """Load dictionary into a list and return list"""
-    pass
+	"""Load dictionary into a list and return list"""
+	with open('dictionary','r') as f:
+		return f.readlines()
 
-def calc_word_value():
-    """Calculate the value of the word entered into function
-    using imported constant mapping LETTER_SCORES"""
-    pass
+def calc_word_value(word):
+	"""Calculate the value of the word entered into function
+	using imported constant mapping LETTER_SCORES"""
+	return reduce(lambda x,y:x+y,[LETTER_SCORES[i.upper] for i in word])
 
-def max_word_value():
-    """Calculate the word with the max value, can receive a list
-    of words as arg, if none provided uses default DICTIONARY"""
-    pass
+def max_word_value(words):
+	"""Calculate the word with the max value, can receive a list
+	of words as arg, if none provided uses default DICTIONARY"""
+	if words=='':
+		res=[(i,calc_word_value(i)) for i in load_words()]
+	else:
+		res = [(i, calc_word_value(i)) for i in words]
+	return sorted(res,key=lambda x:x[1])
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+	tst=TestWordValue()
+    tst.
+	pass # run unittests to validate
